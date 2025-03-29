7702bcbf51bbf5fb36825cbd6b1d82e54c81432a
RockCoder108 <samyak80jan@gmail.com>
2025-03-29 22:54:32 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+
+function App() {
+  return (
+    <>
+    <Router>
+
+
+      <Routes>
+        {/* <Route path='/home' elem */}
+
+        <Route path='/' element = {<LandingPage />} />
+        <Route path='/auth' element = {<Authentication/>} />
+
+      </Routes>
+
+    </Router>
+    
+    </>
+  );
+}
+
+export default App;
